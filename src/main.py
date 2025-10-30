98daf54a4a62b438279f32f1c955d51a7b462667
elfahad98 <elfahad180@gmail.com>
2025-10-30 19:36:57 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 """ 
 Auteur : COMBO EL-fahad 
-Date de création : 23 Decembre 2023
+Date de création : 22 Decembre 2023
 
 """
 
@@ -101,4 +101,5 @@ if __name__ == "__main__":
     master = tk.Tk()
     app = MyApp(master)
     master.mainloop()
+
 
